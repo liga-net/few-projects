c4c6a04a44121e84fe92790b7ebb25fa0ec02078
liga-net <70636479+liga-net@users.noreply.github.com>
2021-03-29 10:50:03 +0300

    Add files via upload

---
+++ gas_firtash.js
@@ -0,0 +1,351 @@
+var graph = {
+    "nodes":[
+    {"node":0,"name":"Сумыгаз"},
+{"node":1,"name":"Житомиргаз"},
+{"node":2,"name":"Винницагаз"},
+{"node":3,"name":"Днепропетровскгаз"},
+{"node":4,"name":"Волыньгаз"},
+{"node":5,"name":"Киевоблгаз"},
+{"node":6,"name":"Запорожгаз"},
+{"node":7,"name":"Закарпатгаз"},
+{"node":8,"name":"Ивано-Франковскгаз"},
+{"node":9,"name":"Криворожгаз"},
+{"node":10,"name":"Львовгаз"},
+{"node":11,"name":"Николаевгаз"},
+{"node":12,"name":"Ровногаз"},
+{"node":13,"name":"Харьковгоргаз"},
+{"node":14,"name":"Харьковгаз"},
+{"node":15,"name":"Тисменицагаз"},
+{"node":16,"name":"Черниговгаз"},
+{"node":17,"name":"Хмельницкгаз"},
+{"node":18,"name":"Днипрогаз"},
+{"node":19,"name":"Черновцыгаз"},
+{"node":20,"name":"Луганскгаз"},
+{"node":21,"name":"НАК Нафтогаз Украины"},
+{"node":22,"name":"Газтек"},
+{"node":23,"name":"Омега-Капитал"},
+{"node":24,"name":"Агропромкомплект"},
+{"node":25,"name":"Ролюкс"},
+{"node":26,"name":"Транзит-інвест"},
+{"node":27,"name":"Центрагаз Холдинг Лимитед"},
+{"node":28,"name":"ХІМАЛІТА ЛІМІТЕД"},
+{"node":29,"name":"Добрый капитал"},
+{"node":30,"name":"Ласфано Украина"},
+{"node":31,"name":"Січкар Сергій Володимирович"},
+{"node":32,"name":"ERISWELL TRADING LIMITED"},
+{"node":33,"name":"TANTER HOLDINGS LIMITED"},
+{"node":34,"name":"HUMGATE HOLDINGS LIMITED"},
+{"node":35,"name":"SAGACITY LIMITED"},
+{"node":36,"name":"SODEMAN LIMITED"},
+{"node":37,"name":"Matheron Limited"},
+{"node":38,"name":"Луняка Владимир Александрович"},
+{"node":39,"name":"Кабинет министров"},
+{"node":40,"name":"Pasler Enterprises"},
+{"node":41,"name":"Krezer Holding Limited"},
+{"node":42,"name":"Nesiba Venchers Limitd"},
+{"node":43,"name":"Porala Venchers Limited"},
+{"node":44,"name":"Дмитрий Затовка"},
+{"node":45,"name":"Промышленные инвестиции"},
+{"node":46,"name":"Финэксперт"},
+{"node":47,"name":"Дмитрий Фирташ"},
+{"node":48,"name":"Арун Кумар Джайрам"},
+{"node":49,"name":"Андрей Полевский"},
+{"node":50,"name":"Александр Анпилов"},
+{"node":51,"name":"Роман Мандригеля"}
+        
+    ],
+    "links":[
+    {"target":0,"source":21,"value":25},
+{"target":0,"source":22,"value":46.64},
+{"target":0,"source":23,"value":24.2},
+{"target":1,"source":22,"value":50.4},
+{"target":1,"source":17,"value":24.3},
+{"target":1,"source":31,"value":5.5},
+{"target":2,"source":21,"value":25},
+{"target":2,"source":22,"value":48.8},
+{"target":2,"source":24,"value":19},
+{"target":2,"source":25,"value":5},
+{"target":3,"source":21,"value":25},
+{"target":3,"source":22,"value":48.8},
+{"target":4,"source":22,"value":48.8},
+{"target":4,"source":26,"value":24.6},
+{"target":5,"source":21,"value":25},
+{"target":5,"source":23,"value":20.4},
+{"target":5,"source":26,"value":19},
+{"target":6,"source":21,"value":25},
+{"target":6,"source":22,"value":50},
+{"target":6,"source":27,"value":14.5},
+{"target":7,"source":23,"value":21},
+{"target":7,"source":26,"value":24},
+{"target":7,"source":22,"value":21.2},
+{"target":8,"source":22,"value":30},
+{"target":8,"source":21,"value":25},
+{"target":8,"source":25,"value":15.3},
+{"target":8,"source":24,"value":15.3},
+{"target":9,"source":22,"value":7},
+{"target":9,"source":13,"value":10.75},
+{"target":9,"source":32,"value":17.8},
+{"target":9,"source":33,"value":15},
+{"target":9,"source":34,"value":21},
+{"target":9,"source":35,"value":12.5},
+{"target":9,"source":36,"value":13.8},
+{"target":10,"source":22,"value":25},
+{"target":10,"source":21,"value":25},
+{"target":10,"source":37,"value":22.85},
+{"target":11,"source":22,"value":50.2},
+{"target":11,"source":21,"value":25},
+{"target":12,"source":22,"value":47.75},
+{"target":12,"source":21,"value":25},
+{"target":12,"source":37,"value":24.8},
+{"target":13,"source":22,"value":9.9},
+{"target":13,"source":32,"value":20.6},
+{"target":13,"source":34,"value":19.2},
+{"target":13,"source":33,"value":18.9},
+{"target":13,"source":28,"value":7.6},
+{"target":14,"source":32,"value":14.53},
+{"target":14,"source":33,"value":13.6},
+{"target":14,"source":34,"value":19.73},
+{"target":14,"source":35,"value":13.2},
+{"target":14,"source":30,"value":17.7},
+{"target":15,"source":22,"value":26},
+{"target":15,"source":21,"value":25},
+{"target":15,"source":29,"value":8.5},
+{"target":15,"source":17,"value":24.9},
+{"target":16,"source":23,"value":49.1},
+{"target":16,"source":26,"value":9.7},
+{"target":16,"source":22,"value":19.7},
+{"target":17,"source":22,"value":51},
+{"target":17,"source":21,"value":25},
+{"target":17,"source":38,"value":14.7},
+{"target":18,"source":13,"value":7},
+{"target":18,"source":32,"value":16.6},
+{"target":18,"source":33,"value":16.6},
+{"target":18,"source":34,"value":21.5},
+{"target":18,"source":35,"value":16.9},
+{"target":18,"source":36,"value":9.7},
+{"target":19,"source":22,"value":49.9},
+{"target":19,"source":23,"value":19.7},
+{"target":19,"source":26,"value":9.7},
+{"target":20,"source":21,"value":25},
+{"target":20,"source":22,"value":49.9},
+{"target":20,"source":23,"value":9},
+{"target":20,"source":26,"value":7.73},
+{"target":21,"source":39,"value":100},
+{"target":22,"source":40,"value":24},
+{"target":22,"source":41,"value":24},
+{"target":22,"source":42,"value":24},
+{"target":22,"source":43,"value":24},
+{"target":23,"source":40,"value":20},
+{"target":23,"source":41,"value":20},
+{"target":23,"source":42,"value":20},
+{"target":23,"source":43,"value":20},
+{"target":23,"source":37,"value":20},
+{"target":22,"source":40,"value":24},
+{"target":22,"source":41,"value":24},
+{"target":22,"source":42,"value":24},
+{"target":22,"source":43,"value":24},
+{"target":24,"source":44,"value":100},
+{"target":25,"source":45,"value":42.2},
+{"target":25,"source":37,"value":3.6},
+{"target":25,"source":30,"value":1},
+{"target":25,"source":46,"value":1.5},
+{"target":25,"source":42,"value":20},
+{"target":25,"source":36,"value":27.1},
+{"target":25,"source":35,"value":11.1},
+{"target":25,"source":33,"value":14.8},
+{"target":26,"source":40,"value":20},
+{"target":26,"source":41,"value":20},
+{"target":26,"source":42,"value":20},
+{"target":26,"source":43,"value":20},
+{"target":26,"source":37,"value":20},
+{"target":27,"source":47,"value":90},
+{"target":28,"source":48,"value":100},
+{"target":29,"source":49,"value":50},
+{"target":29,"source":50,"value":50},
+{"target":30,"source":51,"value":100}
+    ]}
+    // set the dimensions and margins of the graph
+    sankey(window.innerWidth)
+    window.onresize = function(event) {
+        sankey(window.innerWidth)
+    };
+    function sankey(w) {
+        if (w < 600) {
+            var localwidth = w,
+            localheight = 800;
+        } else {
+            var localwidth = 600,
+            localheight = 650
+        }
+    var margin = {top: 10, right: 10, bottom: 10, left: 10},
+        width = localwidth - margin.left - margin.right,
+        height = localheight - margin.top - margin.bottom;
+    
+    // append the svg object to the body of the page
+    d3.select("#my_dataviz").html('')
+    var svg = d3.select("#my_dataviz").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+    
+    // Color scale used
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+    
+    // Set the sankey diagram properties
+    var sankey = d3.sankey()
+        .nodeWidth(20)
+        .nodePadding(4)
+        .size([width, height]);
+    
+    
+      // Constructs a new Sankey generator with the default settings.
+      sankey
+          .nodes(graph.nodes)
+          .links(graph.links)
+          .layout(1);
+          var tooltipBee = d3.select("body")
+                .append("div")
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("visibility", "hidden")
+                .style("padding", "14px")
+                .style("background-color", "white")
+                .style("border", "1px solid #f5f5f5")
+                .style("color", "black")
+                .style("border-radius", "6px")
+                .style("border-color", "black")
+                .style("font", "12px sans-serif")
+                .text("tooltip");
+
+      var link = svg.append("g")
+        .selectAll(".link")
+        .data(graph.links)
+        .enter()
+        .append("path")
+          .attr("class", "link")
+          .attr("d", sankey.link() )
+          .style("stroke-width", function(d) { return 10; })
+          .style("fill", "none")
+          .style("stroke-opacity", .2)
+          .style("stroke", function(d) { 
+              if (d.source.name.includes('газ') == true && d.source.name != 'НАК Нафтогаз Украины') {
+                return '#2e7d32'
+              } else {
+                return d.color = color(d.source.name.replace(/ .*/, "")); 
+              }
+              
+            })
+          .on("mouseover", function(d) {
+                d3.select(this).style('stroke-opacity',.7);
+                tooltipBee.html(
+                    '<p><b>'+d.source.name+'</b> владеет '+String(d.value)+'% акций в компании <b>"'+d.target.name+'"</b></p>'
+                )
+                return tooltipBee.style("visibility", "visible")
+            })
+            .on("mousemove", function() {
+              d3.select(this).style('stroke-opacity',.7);
+                return tooltipBee.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+            })
+            .on("mouseout", function(d){
+              d3.select(this).style('stroke-opacity',.2);
+                return tooltipBee.style("visibility", "hidden");
+            })
+          .sort(function(a, b) { return b.dy - a.dy; });
+          
+      // add in the nodes
+      var node = svg.append("g")
+        .selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("g")
+          .attr("class", "node")
+          .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+    
+      // add the rectangles for the nodes
+      node
+        .append("rect")
+          .attr("height", function(d) { return d.dy; })
+          .attr("width", sankey.nodeWidth())
+          .style("fill", function(d) { 
+              if (d.name.includes('газ') == true && d.name != 'НАК Нафтогаз Украины') {
+                return '#2e7d32'
+              } else {
+                return d.color = color(d.name.replace(/ .*/, "")); 
+              }
+              
+            })
+          .on("mouseover", function(d) {
+                d3.select(this).style('stroke','black');
+                tooltipBee.html(
+                    '<h4>'+d.name+'</h4>'+
+                    '<p>Акционер в '+String(d.sourceLinks.length)+' компаниях</p>'
+                )
+                return tooltipBee.style("visibility", "visible")
+            })
+            .on("mousemove", function() {
+                d3.select(this).style('stroke','black');
+                return tooltipBee.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");
+            })
+            .on("mouseout", function(d){
+              d3.select(this).style('stroke','none');
+                return tooltipBee.style("visibility", "hidden");
+            })
+    
+      // add in the title for the nodes
+      var mob_names = ['НАК Нафтогаз Украины','Газтек','Омега-Капитал','Транзит-інвест',"Pasler Enterprises","Krezer Holding Limited","Nesiba Venchers Limitd","Porala Venchers Limited","Сумыгаз","Житомиргаз","Винницагаз","Днепропетровскгаз","Волыньгаз","Киевоблгаз","Запорожгаз","Закарпатгаз","Ивано-Франковскгаз","Криворожгаз","Львовгаз","Николаевгаз","Ровногаз","Харьковгаз","Тисменицагаз","Черниговгаз","Днипрогаз","Черновцыгаз","Луганскгаз",'Matheron Limited','SAGACITY LIMITED','HUMGATE HOLDINGS LIMITED','Дмитрий Фирташ']
+        node
+          .append("text")
+            .attr("x", -6)
+            .attr("y", function(d) { return d.dy / 4; })
+            .attr("dy", ".35em")
+            .attr("class", function(d) {
+              if (d.name.includes('газ') == true&& d.name != 'НАК Нафтогаз Украины') {
+                return 'gaz'
+              } else {
+                return 'name'
+              }
+            })
+            .attr("text-anchor", "end")
+            .attr('font-size','14px')
+            .attr("transform", null)
+            .text(function(d) { 
+                if (width > 600) {
+                    return d.name; 
+                } else if (mob_names.includes(d.name)){
+                    return d.name 
+                }
+            })
+            
+          .filter(function(d) { return d.x < width / 2; })
+            .attr("x", 6 + sankey.nodeWidth())
+            .attr("text-anchor", "start");
+          if (width < 600) {
+              node.selectAll(".name")
+              .call(wrap, 100);
+            }
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 25).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 25).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+    
+}

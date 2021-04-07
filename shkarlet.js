223d2551bca6aa0e19344fc6f2458c69725fe408
liga-net <70636479+liga-net@users.noreply.github.com>
2021-04-07 14:37:15 +0300

    Add files via upload

---
+++ shkarlet.js
@@ -0,0 +1,22 @@
+if(document.createStyleSheet) {
+    document.createStyleSheet('https://cdn.jsdelivr.net/gh/liga-net/few-projects/shkarlet.css');
+   }
+   else {
+    var styles = "@import url('https://cdn.jsdelivr.net/gh/liga-net/few-projects/shkarlet.css');";
+    var newSS=document.createElement('link');
+    newSS.rel='stylesheet';
+    newSS.href='data:text/css,'+escape(styles);
+    document.getElementsByTagName("head")[0].appendChild(newSS);
+}
+function clickOnButton(element) {
+    var className = element.className.split(' ')[1]
+    $('#'+className+'_text').css('display','block')
+    $('#'+className+'_2_text').css('display','block')
+    $('.'+className).css('display','none')
+ }
+ $('.sh_show').click(function() {
+    clickOnButton(this)
+ })
+ $('.sh_details').click(function() {
+    clickOnButton(this)
+ })
